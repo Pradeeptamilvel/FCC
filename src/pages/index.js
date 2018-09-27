98d55ed12a59e819275b7f498d6efb9a05791393
Pradeeptamilvel <tamilvelpradeep583@gmail.com>
2018-09-27 17:20:24 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>I'm Pradeep</h1>
+    <p>I'm studying in K.G college</p>
+    <p>I love coding</p>
+    <p>I love my country</p>
+  </Layout>
+)
+
+export default IndexPage
